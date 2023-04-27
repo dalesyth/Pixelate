14209c0b110e0af9f6601a705ee5db62d34aa0fe
Dale Syth <dalesyth@gmail.com>
2023-04-26 20:51:45 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your code here
+const table = document.getElementsByTagName("table")[0];
+const addRow = document.getElementById("add-row");
+addRow.addEventListener("click", makeRow);
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    let td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+
+makeRow();
